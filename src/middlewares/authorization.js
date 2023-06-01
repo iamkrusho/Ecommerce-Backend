e79699696c8bbdeaf14c5418e994cb67f41b9e20
Ignacio Kruchowski <ignakruchowski@gmail.com>
2023-06-01 10:54:25 -0300

    feat: add authorization middleware

---
+++ src/middlewares/authorization.js
@@ -0,0 +1,14 @@
+function authorization(permission) {
+    return async function(req, res, next) {
+        const user = req.user;
+        const hasPermission = user.role?.permissions.includes(permission);
+
+        if (!(user.isAdmin && hasPermission)) {
+            return res.status(401).send({ status: "error", message: "Not authorization" })
+        }
+
+        next();
+      }
+}
+
+export default authorization;
