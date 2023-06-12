284651873775d892d86394c9256bdb1fcfd688b5
Ignacio Kruchowski <ignakruchowski@gmail.com>
2023-06-12 10:34:07 -0300

    feat: add AppFactory and AppExpress

--- src/index.js
+++ src/index.js
@@ -1,32 +1,14 @@
-import express from "express";
-import cookieParser from "cookie-parser";
+import AppFactory from "./presentation/factories/appFactory.js"
 
-import productsRouter from "./presentation/routes/productsRouter.js";
-import cartRouter from "./presentation/routes/cartsRouter.js"
-import usersRouter from "./presentation/routes/usersRouter.js"
-import rolesRouter from "./presentation/routes/rolesRouter.js";
-import sessionRouter from "./presentation/routes/sessionRouter.js"
-
-import errorHandler from "./presentation/middlewares/errorHandler.js";
 import { connectDB } from "./config/db.config.js";
 
+
 void (async () => {
-    const app = express();
-    
     await connectDB();
 
-    app.use(express.json());
-    app.use(express.urlencoded({ extended: true }));
-    app.use(cookieParser());
+    const app = AppFactory.create();
     
-    app.use("/api/products", productsRouter);
-    app.use("/api/carts", cartRouter);
-    app.use("/api/users", usersRouter);
-    app.use("/api/roles", rolesRouter);
-    app.use("/api/session", sessionRouter);
-    app.use(errorHandler);
-
-    app.listen(8080, () => {
-        console.log("Listening on port 8080");
-    });
+    app.init();
+    app.build();
+    app.listen();
 })();
---
+++ src/presentation/application/appExpress.js
@@ -0,0 +1,36 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+
+import productsRouter from "../routes/productsRouter.js";
+import cartRouter from "../routes/cartsRouter.js"
+import usersRouter from "../routes/usersRouter.js"
+import rolesRouter from "../routes/rolesRouter.js";
+import sessionRouter from "../routes/sessionRouter.js"
+
+import errorHandler from "../middlewares/errorHandler.js";
+
+class AppExpress {
+    init() {
+        this.app = express();
+        this.app.use(express.json());
+        this.app.use(express.urlencoded({ extended: true }));
+        this.app.use(cookieParser());
+    }
+
+    build() {
+        this.app.use("/api/products", productsRouter);
+        this.app.use("/api/carts", cartRouter);
+        this.app.use("/api/users", usersRouter);
+        this.app.use("/api/roles", rolesRouter);
+        this.app.use("/api/session", sessionRouter);
+        this.app.use(errorHandler);
+    }
+
+    listen() {
+        return this.app.listen(8080, () => {
+            console.log("Listening on port 8080");
+        });
+    }
+}
+
+export default AppExpress;
