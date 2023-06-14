269365d121316b9c801ee9caa52e73a0b6bd82cb
Ignacio Kruchowski <ignakruchowski@gmail.com>
2023-06-14 12:00:48 -0300

    feat: add domain entities

---
+++ src/domain/entities/product.js
@@ -0,0 +1,15 @@
+class Product {
+    constructor(props) {
+        this.id = props.id;
+        this.title = props.title;
+        this.description = props.description;
+        this.code = props.code;
+        this.price = props.price;
+        this.status = props.status;
+        this.stock = props.stock;
+        this.category = props.category;
+        this.thumbnails = props.thumbnails;
+    }
+}
+
+export default Product;
---
+++ src/domain/entities/user.js
@@ -0,0 +1,14 @@
+class User {
+    constructor(props) {
+        this.id = props.id;
+        this.firstName = props.firstName;
+        this.lastName = props.lastName;
+        this.email = props.email;
+        this.age = props.age;
+        this.role = props.role;
+        this.isAdmin = props.isAdmin;
+        this.password = props.password;
+    }
+}
+
+export default User;
