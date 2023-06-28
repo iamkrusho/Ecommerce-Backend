a0787ab567769798bca9ea1b7b9341a3e5c58c1c
Ignacio Kruchowski <ignakruchowski@gmail.com>
2023-06-28 15:31:18 -0300

    feat: add new methods to TicketManager and TicketMongooseRepository

--- src/data/repositories/ticketMongooseRepository.js
+++ src/data/repositories/ticketMongooseRepository.js
@@ -2,6 +2,30 @@ import TicketModel from "../models/ticketModel.js";
 import Ticket from "../../domain/entities/ticket.js";
 
 class TicketMongooseRepository {
+    async find() {
+        const ticketsDocs = await TicketModel.find();
+
+        return (!ticketsDocs > 0) ? null : ticketsDocs.map((doc) => new Ticket({
+            id: doc._id,
+            code: doc.code,
+            date: doc.date,
+            total: doc.total,
+            user: doc.user
+        }));
+    }
+
+    async findOne(id) {
+        const ticketDoc = await TicketModel.findById(id);
+
+        return (!ticketDoc) ? null : new Ticket({
+            id: ticketDoc._id,
+            code: ticketDoc.code,
+            date: ticketDoc.date,
+            total: ticketDoc.total,
+            user: ticketDoc.user
+        });
+    }
+
     async save(data) {
         const newTicketDoc = new TicketModel(data);
         await newTicketDoc.save();
@@ -13,6 +37,12 @@ class TicketMongooseRepository {
             total: newTicketDoc.total,
             user: newTicketDoc.user
         });
+    }
+
+    async delete(id) {
+        const ticketDoc = await TicketModel.findByIdAndDelete(id);
+
+        return (!ticketDoc) ? null : true;
     }
 }
 
--- src/domain/managers/ticketManager.js
+++ src/domain/managers/ticketManager.js
@@ -5,8 +5,20 @@ import container from "../../container";
 class TicketManager {
     #TicketRepository = container.resolve("TicketRepository");
 
+    async getAll() {
+        return await this.#TicketRepository.find();
+    }
+
+    async getOne(id) {
+        return await this.#TicketRepository.findOne(id);
+    }
+
     async create(data) {
         return await this.#TicketRepository.save({...data, code: nanoid(13)})
+    }
+
+    async deleteOne(id) {
+        return await this.#TicketRepository.delete(id);
     }
 }
 
